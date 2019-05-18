93559e7f93cb3dabcfd56d774fe359772edd7485
v.roudge <valentin@carts.guru>
2019-05-18 13:27:21 -0700

    fix

--- server/graph/graphql.js
+++ server/graph/graphql.js
@@ -3,7 +3,7 @@
  */
 
 import koaGraphQL from 'koa-graphql'
-import { makeExecutableSchema } from 'graphql-tools/dist/index'
+import { makeExecutableSchema } from 'graphql-tools'
 
 import resolvers from './resolvers'
 import typeDefs  from './schema'
