2aeaf6b35ecf01ccb93ce32afdb531ec05a0a381
v.roudge <valentin@carts.guru>
2019-05-18 16:20:42 -0700

    derps

--- server/controllers/config.js
+++ server/controllers/config.js
@@ -5,22 +5,12 @@
 export const configController = async ctx => {
   const { body } = ctx.request
   ctx.assert(body.id, 400, 'NO_ID_PROVIDED')
-  /*
-  {
-    "available": true,
-    "max_output": 10,
-    "max_output_duration": 10,
-    "current_command": {
-      "magnitude": 0,
-      "duration": 0,
-      "command_timeout": 0
-    }
-  }
-   */
+
   ctx.body = {
     objectId: '',
     villageId: '',
     type: '',
+    legacy: true,
     values: [],
     commandable: true
   }
