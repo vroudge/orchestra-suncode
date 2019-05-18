57d8673cc2c851b83ba59c73be5c2ec1f00f7be0
v.roudge <valentin@carts.guru>
2019-05-17 22:42:06 -0700

    Add babel

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+const config = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
+}
+
+module.exports = config
