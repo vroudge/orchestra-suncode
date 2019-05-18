95fe3ae3f35dad5d5c5b737dfc5f4d7db76ac0eb
Romain <romain.b@carts.guru>
2019-05-18 11:34:26 -0700

    front wip

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
-
 import Vue from 'vue'
-import apolloProvider from './apollo'
+import apolloProvider from './api'
 
 import App from './App.vue'
 
@@ -8,6 +7,6 @@ const vm = new Vue({
   apolloProvider,
   el: '#app',
   render: h => h(App)
-})
+});
 
 export default vm
